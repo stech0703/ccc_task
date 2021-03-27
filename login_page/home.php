938ee4e752239b6c495c897df248b3e2d2d8752b
stech0703 <s.tech0703@gmail.com>
2021-03-28 01:42:30 +0530

    login page task

---
+++ login_page/home.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+
+
+?>
+<html>
+<head>
+<title>home page</title>
+
+</head>
+<body>
+<a href="logout.php">LOGOUT</a>
+    <h1>Welcome</h1>
+
+</body>
+
+
+
+
+
+
+
+
+
+</html>
+
+
+
